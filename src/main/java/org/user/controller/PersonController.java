9b0355d484f52e9e88a4b7f9033f3c721d2367df
Dashrath Chauhan <dashrathcomp@gmail.com>
2021-02-12 12:41:50 +0530

    README updated, Response code changed for some APIs

--- src/main/java/org/user/controller/PersonController.java
+++ src/main/java/org/user/controller/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.user.model.Person;
 import org.user.service.PersonService;
@@ -37,6 +39,7 @@ public class PersonController {
     }
 
     @PostMapping("/persons")
+    @ResponseStatus(HttpStatus.CREATED) 
     public List<Person> addPerson(@RequestBody List<Person> persons) {
     	logger.info("Adding new persons.");
     	return personService.addPerson(persons);
